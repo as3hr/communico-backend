3221232ff2ad48add304f127542415f9cad72c90
Asher <asheressani@gmail.com>
2024-12-17 22:54:08 +0500

    WorkFlow Updation:7 !

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -8,6 +8,10 @@ export async function dbConnection() {
     console.log("✅ Database connected successfully!");
   } catch (error) {
     console.error("❌ Error connecting to the database:", error);
+    if (error instanceof Error) {
+      console.error("Error message:", error.message);
+      console.error("Error stack:", error.stack);
+    }
     process.exit(1);
   }
 }
